c1a8f3e0a1895103ef31815c9c66fe18f126ce22
lp0ne <>
2025-10-30 13:39:27 +0800

    chore: add version control files and configuration

---
+++ weapp-template/config/config/config/env.js
@@ -0,0 +1,5 @@
+// 本地真实配置
+export default {
+    cloudEnv: "cloud1-xxxxxx" // ← 你的真实云环境 ID
+  }
+  
---
+++ weapp-template/config/config/env.example.js
@@ -0,0 +1,5 @@
+// 示例配置（提交到仓库）
+export default {
+    cloudEnv: "your-cloud-env-id" // ← 替换为你自己的云环境 ID
+  }
+  
