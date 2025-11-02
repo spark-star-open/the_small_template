0c33dcad73dea18e7549d3dd3ecc8da23669835c
lp0ne <2509266472@qq.com>
2025-11-02 22:54:19 +0800

    Delete weapp-template/weapp-template directory

--- weapp-template/weapp-template/config/env.example.js
+++
@@ -1,5 +0,0 @@
-// 示例配置（提交到仓库）
-export default {
-    cloudEnv: "your-cloud-env-id" // ← 替换为你自己的云环境 ID
-  }
-  
--- weapp-template/weapp-template/config/env.js
+++
@@ -1,5 +0,0 @@
-// 本地真实配置
-export default {
-    cloudEnv: "cloud1-3gcn7rgkab20f4ae" // ← 你的真实云环境 ID
-  }
-  
