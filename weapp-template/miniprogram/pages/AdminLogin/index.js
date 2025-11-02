45b038f47c5afea36493a56259e463ad47b2a083
Shi Boan <shiboan81@gmail.com>
2025-11-02 23:34:08 +0800

    feat: 本次修改的简要说明

--- weapp-template/miniprogram/pages/AdminLogin/index.js
+++ weapp-template/miniprogram/pages/AdminLogin/index.js
@@ -1,26 +1,42 @@
-const storage = require('../../../utils/storage');
-const auth = require('../../../utils/auth');
-
+const storage = require('../../utils/storage');
 
 Page({
-data: { pwd: '', show: false, loading: false },
-onInput(e) { this.setData({ pwd: e.detail.value }); },
-toggleShow() { this.setData({ show: !this.data.show }); },
-onLogin() {
-if (this.data.loading) return;
-if (!this.data.pwd) { wx.showToast({ title: '请输入密码', icon: 'none' }); return; }
-this.setData({ loading: true });
+  data: {
+    pwd: '',
+    show: false,
+    toggleText: '显示',
+    loading: false
+  },
 
+  onInput(e) {
+    this.setData({ pwd: e.detail.value });
+  },
 
-// TODO: 替换为后端校验；当前仅做前端演示
-setTimeout(() => {
-this.setData({ loading: false });
-if (this.data.pwd === '123456') { // 占位密码
-storage.set('adminToken', 'demo-token');
-wx.reLaunch({ url: '/pages/AdminDashboard/index' });
-} else {
-wx.showToast({ title: '密码错误', icon: 'none' });
-}
-}, 600);
-}
+  toggleShow() {
+    const nextShow = !this.data.show;
+    this.setData({
+      show: nextShow,
+      toggleText: nextShow ? '隐藏' : '显示'
+    });
+  },
+
+  onLogin() {
+    if (this.data.loading) return;
+    if (!this.data.pwd) {
+      wx.showToast({ title: '请输入密码', icon: 'none' });
+      return;
+    }
+    this.setData({ loading: true });
+
+    // TODO: 接后端验证；此处仅演示
+    setTimeout(() => {
+      this.setData({ loading: false });
+      if (this.data.pwd === '123456') {
+        storage.set('adminToken', 'demo-token');
+        wx.reLaunch({ url: '/pages/AdminDashboard/index' });
+      } else {
+        wx.showToast({ title: '密码错误', icon: 'none' });
+      }
+    }, 500);
+  }
 });
